ad057ebc59e5c3b8aa1e924ec5d3cfcfe894a9ce
Yadira Ferrer <yyfm.95@gmail.com>
2023-05-14 17:45:44 -0600

    Add VPC and subnet configuration

--- 01-Intro-Terraform/main.tf
+++ 01-Intro-Terraform/main.tf
@@ -11,3 +11,25 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+# Configuration of VPC
+resource "aws_vpc" "yf_vpc_example" {
+  cidr_block = "25.0.0.0/16"
+  tags = {
+    Name = "yf-tf-vpc"
+  }
+}
+
+# Configuration of public subnet
+resource "aws_subnet" "yf_public_subnet" {
+  vpc_id = aws_vpc.yf_vpc_example.id
+  cidr_block = "25.0.1.0/24"
+  tags = {
+    Name = "yf-tf-public-1"
+  }
+}
+
+# terraform destroy -> delete all the infraestructure created.
+
+
+
